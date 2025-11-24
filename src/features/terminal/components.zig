6d56995da7c8565b9dc56cafaf09bb87b245383a
LeviathanST <phamquochung170505@gmail.com>
2025-11-24 18:21:20 +0700

    terminal/cmd_executor: add docs; timing to start the timer

--- src/features/terminal/components.zig
+++ src/features/terminal/components.zig
@@ -8,12 +8,14 @@ const Command = Interpreter.Command;
 
 pub const Terminal = struct {};
 
+/// All commands will be executed in FIFO order
 pub const CommandExecutor = struct {
     queue: Queue,
     alloc: std.mem.Allocator,
     /// The timestamp of the previous command execution
     /// in miliseconds.
-    timer: std.time.Timer,
+    /// The timer is started from the first commmands is added.
+    timer: ?std.time.Timer = null,
     _arena: *std.heap.ArenaAllocator,
     is_running: bool = false,
 
@@ -31,7 +33,6 @@ pub const CommandExecutor = struct {
             .queue = .{},
             .alloc = arena.allocator(),
             ._arena = arena,
-            .timer = try .start(),
         };
     }
 
@@ -46,6 +47,7 @@ pub const CommandExecutor = struct {
         it.* = .{ .data = cmd };
 
         if (self.queue.first == null) {
+            self.timer = try .start();
             self.queue.first = &it.node;
         } else {
             var curr_node = self.queue.first.?;
@@ -72,18 +74,22 @@ pub const CommandExecutor = struct {
         /// (miliseconds)
         duration: u64,
     ) !void {
-        const target_ns = duration * std.time.ns_per_ms;
-        const lap = self.timer.read();
+        if (self.timer) |*timer| {
+            const target_ns = duration * std.time.ns_per_ms;
+            const lap = timer.read();
 
-        if (lap > target_ns) {
-            if (self.dequeue()) |command| {
-                self.is_running = true;
-                self.timer.reset();
-                try switch (command) {
-                    .move => |direction| digger.action.control(w, direction),
-                };
-            } else {
-                self.is_running = false;
+            if (lap > target_ns) {
+                if (self.dequeue()) |command| {
+                    self.is_running = true;
+                    timer.reset();
+
+                    try switch (command) {
+                        .move => |direction| digger.action.control(w, direction),
+                    };
+                } else {
+                    self.timer = null;
+                    self.is_running = false;
+                }
             }
         }
     }
