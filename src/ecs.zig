b5051f741767a22dfbefe7479722923e722f119a
LeviathanST <phamquochung170505@gmail.com>
2026-01-06 17:06:28 +0700

    remove an unintended import

--- src/ecs.zig
+++ src/ecs.zig
@@ -7,8 +7,6 @@ pub const ComponentStorage = component.Storage;
 pub const World = @import("ecs/World.zig");
 pub const Entity = @import("ecs/Entity.zig");
 
-pub const SystemConfig = @import("ecs/system.zig").Config;
-
 pub const common = @import("ecs/common.zig");
 pub const CommonModule = common.CommonModule;
 
